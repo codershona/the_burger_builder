a2cab9a799ca9068f276979c80fdf83e59926d4c
codershona <falgunimst95@gmail.com>
2020-03-12 18:56:58 +0100

    Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+
+import Layout from './components/Layout/Layout';
+
 
 
 class App extends Component {
   render() {
     return (
        <div>
-
-
+       <Layout>
+       <p> Test</p>
+      </Layout>
           </div>
 
 
