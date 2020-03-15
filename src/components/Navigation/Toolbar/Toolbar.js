a26105932ac7dd77d26813986ca980b19ef03fe6
codershona <falgunimst95@gmail.com>
2020-03-15 17:37:31 +0100

    Using a Logo in my Application done

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-           <div>MENU</div>
-           <div>LOGO</div>
+           
+            <div>MENU</div>
+
+            <Logo />
+            
            <nav>
              ...
 
