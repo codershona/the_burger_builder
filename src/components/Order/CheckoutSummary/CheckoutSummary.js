bfe1689a179fec2b7f5b03226c4d5001bcf725f8
codershona <falgunimst95@gmail.com>
2020-03-24 17:16:42 +0100

    Building the Checkout Container styled done

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,7 +10,7 @@ const checkoutSummary = (props) => {
 	return (
       <div className={classes.CheckoutSummary}>
        <h1>We hope it tastes well!</h1>
-       <div style={{width: '100%', height: '300px', margin: 'auto'}}>
+       <div style={{width: '100%', margin: 'auto'}}>
        <Burger ingredients={props.ingredients} />
     
 
