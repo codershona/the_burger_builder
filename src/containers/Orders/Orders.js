d47285994fe188deb608f418ec895d6c4bb3e802
codershona <falgunimst95@gmail.com>
2020-03-25 14:02:48 +0100

    Fetching Orders : method 1

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
-
+import axios from '../../axios-orders';
 
 
 class Orders extends Component {
+    state = {
+    	orders: [],
+    	loading: true
+    }
+
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(res => {
+        	console.log(res.data);
+        	
+             this.setState({loading: false});
+
+        })
+        .catch(err => {
+        	 this.setState({loading: false});
+
+        });
+    }
+
+
 	render () {
 		return (
 			<div>
