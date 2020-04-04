5c0515063329bb02f916b644dbff24d74b9cb2e9
codershona <falgunimst95@gmail.com>
2020-04-04 14:34:12 +0200

    Redirect to Improve UX done

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 
 import { connect } from 'react-redux';
@@ -8,34 +8,7 @@ import { connect } from 'react-redux';
 
 
 class Checkout extends Component {
-	// state = {
-	// 	ingredients: null,
-	// 	price: 0
-		
-	// }
 
-	// componentWillMount() {
-	// 	const query = new URLSearchParams(this.props.location.search);
-	// 	const ingredients = {};
-	// 	let price = 0;
-
-	// 	for ( let param of query.entries()) {
-	// 		// ['salad', '1']
-
-	// 		if (param[0] === 'price') {
- //               price = param[1];
-
-	// 		} else {
-	// 			ingredients[param[0]] = +param[1];
-
-	// 		}
-
-			
-
-	// 	}
-	// 	this.setState({ ingredients: ingredients, totalPrice: price });
-
-	// }
 
 	checkoutCancelledHandler = () => {
        this.props.history.goBack();
@@ -50,23 +23,32 @@ class Checkout extends Component {
 
 
     render() {
+    	let  summary = <Redirect to="/"/>
 
-    	return (
-       <div>
-          <CheckoutSummary 
+    	if (this.props.ings) {
+    		summary = (
+
+    			<div>
+    			 <CheckoutSummary 
           ingredients={this.props.ings}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}  />
-          <Route 
+
+           <Route 
             path={this.props.match.path + '/contact-data'} 
             
           component={ContactData}  
             />
 
-        
-       </div>
+            </div>
 
-       );
+
+    	  );
+
+    	}
+
+    	return summary;
+
     }
 
 
