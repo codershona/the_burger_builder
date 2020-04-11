70d4ef083e72c3158a1d65d98c67e91a0e2aa41b
codershona <falgunimst95@gmail.com>
2020-04-11 19:41:23 +0200

    Using updateObject in the Entire App done

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -10,6 +10,8 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 import { Redirect } from 'react-router-dom';
 
+import { updateObject } from '../../shared/utility';
+
 
 
 class Auth extends Component {
@@ -107,18 +109,20 @@ class Auth extends Component {
     
     inputChangedHandler = (event, controlName) => {
        
-       const updateControls = {
-        ...this.state.controls,
-        [controlName]: {
-        	...this.state.controls[controlName],
-        	  value: event.target.value,
-        	  valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+       const updateControls = updateObject(this.state.controls, {
 
-        	  touched: true
+           [controlName]: updateObject(this.state.controls[controlName], {
 
-        }
 
-      };
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+
+            touched: true
+
+
+           })
+       });
+
       this.setState({controls: updateControls});
 
     }
