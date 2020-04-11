bc2d84f3e2d1fa6d1c82eed7135a7fffc4f934f5
codershona <falgunimst95@gmail.com>
2020-04-11 13:28:17 +0200

    Fixing Connect + Routing Errors done

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -104,22 +104,25 @@ export const setAuthRedirectPath = (path) => {
 export const authCheckState = () => {
     return dispatch => {
         const token = localStorage.getItem('token');
+
          if (!token) {
+
             dispatch(logout());
 
          } else {
-            const expirationTime = new Date(localStorage.getItem('expirationDate'));
-           if (expirationDate > new Date()) {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+           if (expirationDate <= new Date()) {
 
               dispatch(logout());
-              
+
            } else {
+
              const userId = localStorage.getItem('userId');
 
 
               dispatch(authSuccess(token, userId));
 
-              dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+              dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) /1000 ));
 
 
 
