65ebf4b7834b7081f0f0b09e6da381c739e32b80
codershona <falgunimst95@gmail.com>
2020-04-11 19:51:31 +0200

    Sharing the Validation Method done

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -10,7 +10,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 import { Redirect } from 'react-router-dom';
 
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 
 
@@ -64,49 +64,9 @@ class Auth extends Component {
       this.props.onSetAuthRedirectPath();
 
     }
+}
 
 
-  }
-
-
-
-
-   checkValidity(value, rules) {
-    
-     let isValid = true;
-     if (!rules) {
-        return true;
-     }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-
-     }
-
-     if (rules.minLength) {
-      isValid = value.length >= rules.maxLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.minLength && isValid
-    }
-
-
-  if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-        }
-
-  if (rules.isNumeric) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test(value) && isValid
-    }
-
-
-     return isValid;
-    }
-
-    
     inputChangedHandler = (event, controlName) => {
        
        const updateControls = updateObject(this.state.controls, {
@@ -115,7 +75,7 @@ class Auth extends Component {
 
 
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
 
             touched: true
 
